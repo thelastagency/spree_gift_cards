8322e66603bcfc36f6d50dc14127871bb7272b64
Josh Starcher <josh.starcher@gmail.com>
2011-06-17 11:03:25 -0400

    allow gift card email subject to be customized with locales

--- app/mailers/order_mailer_decorator.rb
+++ app/mailers/order_mailer_decorator.rb
@@ -2,7 +2,7 @@ OrderMailer.class_eval do
   def gift_card_email(card, order)
     @gift_card = card
     @order = order
-    subject = "#{Spree::Config[:site_name]} Gift Card"
+    subject = t('gift_card_messages.email_subject', :site => Spree::Config[:site_name])
     @gift_card.update_attribute(:sent_at, Time.now)
     mail(:to => card.email, :subject => subject)
   end

fd84367f780667d74115a24ef7eeda5cc7dbd54a
jdejong <jdejong@sparknettech.com>
2011-12-07 13:36:48 -0500

    Update db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,6 @@ if Product.where(:is_gift_card => true).count == 0
   option_type.option_values << OptionValue.new(:name => "true")
   product.option_types << option_type
   product.variants = [25, 50, 75, 100].map{|amount| Variant.new(:price => amount)}
+  product.variants.map{|v| v.option_values << option_type.option_values}
   product.save
 end

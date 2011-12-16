65d7d2200d5613d0c8e1445974731129fc5cbeca
Jonathan De Jong <jdejong@sparknettech.com>
2011-12-15 19:30:16 -0500

    change to use logger

--- app/mailers/order_mailer_decorator.rb
+++ app/mailers/order_mailer_decorator.rb
@@ -2,11 +2,11 @@ OrderMailer.class_eval do
   def gift_card_email(card, order)
     @gift_card = card
     @order = order
-    puts '********'
-    puts card.sender || '***'
-    puts card.sender.email || '***'
-    puts card.cc_me || '***'
-    puts '********'
+    logger.info '********'
+    logger.info card.sender || '***'
+    logger.info card.sender.email || '***'
+    logger.info card.cc_me || '***'
+    logger.info '********'
     cc = (card.sender.email if card.cc_me) || ''
     subject = t('spree_gift_card.messages.email_subject', :site => Spree::Config[:site_url], :sender => card.sender_name)
     @gift_card.update_attribute(:sent_at, Time.now)

af057a50af8d7be11697b242612b78f5c0a15c43
Jonathan De Jong <jdejong@sparknettech.com>
2011-12-15 19:17:11 -0500

    debug to find out why cc is missing

--- app/mailers/order_mailer_decorator.rb
+++ app/mailers/order_mailer_decorator.rb
@@ -2,6 +2,11 @@ OrderMailer.class_eval do
   def gift_card_email(card, order)
     @gift_card = card
     @order = order
+    puts '********'
+    puts card.sender || '***'
+    puts card.sender.email || '***'
+    puts card.cc_me || '***'
+    puts '********'
     cc = (card.sender.email if card.cc_me) || ''
     subject = t('spree_gift_card.messages.email_subject', :site => Spree::Config[:site_url], :sender => card.sender_name)
     @gift_card.update_attribute(:sent_at, Time.now)

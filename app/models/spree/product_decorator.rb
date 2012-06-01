2455b1b10d7e548ba5c2ae46fa20e82e9021b248
lostboy <paul.crabtree@gmail.com>
2012-06-01 14:13:43 +0200

    enable mass assignment of is_gift_card for latest rails

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -3,4 +3,6 @@ Spree::Product.class_eval do
   scope :gift_cards, where(["products.is_gift_card = ?", true])
   scope :not_gift_cards, where(["products.is_gift_card = ?", false])
 
+  attr_accessible :is_gift_card
+
 end
